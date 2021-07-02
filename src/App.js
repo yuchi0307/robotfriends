6a41aa68c1598d4ae68c0760545f953905c10027
yuchi0307 <gelee0307@gmail.com>
2021-07-02 18:10:16 +0800

    新增search bar和state

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component }from 'react';
+import CardList from './CardList'
+import SearchBox from './SearchBox'
+import { robots } from './robots' 
+
+class App extends Component {
+	constructor(){
+		super()
+		this.state = {
+		robots: robots,
+		searchfield: ''
+		}
+	}
+
+	onSearchChange(event){
+		console.log(event.target.value);
+	}
+
+	render(){
+		return(
+			<div className='tc'>
+			<h1>ROBOTS FRIENDS</h1>
+			<SearchBox searchChange={this.onSearchChange}/>
+	  		<CardList robots={this.state.robots}/> 
+	  		</div>
+			);
+		}
+	
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
-import { robots } from './robots' 
 //因為 robots 不如 card component是 default export，所以要{}
 ReactDOM.render(
-	<div>
-  		<Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-  		<Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
-  		<Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
-    </div>,
+	<App />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
